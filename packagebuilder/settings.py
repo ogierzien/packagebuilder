7d0c3748eafaa02c4cbf88800d98e985c50f84a2
Ogier Schelvis <ogier@zien24.nl>
2019-07-25 15:16:01 +0200

    gitignore and set redirect_uri

--- packagebuilder/settings.py
+++ packagebuilder/settings.py
@@ -108,7 +108,7 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 SALESFORCE_CONSUMER_KEY = os.environ['SALESFORCE_CONSUMER_KEY']
 SALESFORCE_CONSUMER_SECRET = os.environ['SALESFORCE_CONSUMER_SECRET']
-SALESFORCE_REDIRECT_URI = 'https://packagebuilder.herokuapp.com/oauth_response'
+SALESFORCE_REDIRECT_URI = 'https://salesforce-package-xml-builder.herokuapp.com/oauth_response'
 SALESFORCE_API_VERSION = int(os.environ['SALESFORCE_API_VERSION'])
 
 SALESFORCE_REST_URL = '/services/data/v%d.0/' % SALESFORCE_API_VERSION
