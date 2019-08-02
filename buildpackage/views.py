0205d997ccee4c130b8dd83f490e8ef848518f6e
Ogier Schelvis <ogier@zien24.nl>
2019-08-02 11:22:49 +0200

    xml

--- buildpackage/views.py
+++ buildpackage/views.py
@@ -205,7 +205,7 @@ def package(request, package_id):
 
     package = get_object_or_404(Package, random_id = package_id)
     package_xml = package.package
-    xml = etree.XML(package_xml);
+    xml = etree.tostring(package_xml.text);
     package_xml = etree.tostring(xml, pretty_print=True)
     package.delete()
     return render_to_response('package.html', RequestContext(request, {'package_xml': package_xml}), content_type="text/xml; charset=UTF-8")
