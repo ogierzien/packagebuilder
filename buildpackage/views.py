db69aa367c9d56adbbbae0189bedc9db9e06e443
Ogier Schelvis <ogier@zien24.nl>
2019-08-02 11:56:01 +0200

    xmla nd update python security

--- buildpackage/views.py
+++ buildpackage/views.py
@@ -208,8 +208,7 @@ def package(request, package_id):
     package.delete()
     response = render_to_response('package.html', RequestContext(request, {'package_xml': package_xml}), content_type="text/xml; charset=UTF-8");
     et = etree.fromstring(response.content)
-    pprint(etree.tostring(et, pretty_print=True));
-    return response;
+    return print(etree.tostring(et, pretty_print=True));
 
 
 def logout(request):
